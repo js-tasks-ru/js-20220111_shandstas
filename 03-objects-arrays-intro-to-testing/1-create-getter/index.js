0abe30c6ffd69a381f2c4d4a45d78153dc894a87
shandst <shandstas@mail.ru>
2022-01-20 18:19:25 +0200

    task 3.1

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,18 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+  const fun = (obj) => {
+    let obj_new = {};
 
+    const path2 = path.split('.');
+    if (!obj.hasOwnProperty(path2[0])) return undefined;
+
+    for (let p in path2) {
+      if (Object.keys(obj_new).length == 0) {obj_new = obj[path2[p]];}
+      else obj_new = obj_new[path2[p]];
+    }
+
+    return obj_new;
+  }
+  return fun;
 }
