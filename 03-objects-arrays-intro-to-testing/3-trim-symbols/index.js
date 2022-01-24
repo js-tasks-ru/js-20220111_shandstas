33592d7fdcf9a2111bdebaf4866248a27b19a493
= <=>
2022-01-24 10:51:42 +0200

    task 3.1

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,18 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  if (string === 0) return '';
+  if (size === undefined) return string;
 
+  const arr = Array.from(string);
+
+  const firstSlice = string.slice(0, size);
+  const rest = [...string.slice(size)];
+
+  return rest.reduce((accumString, char) => {
+    if (!accumString.endsWith(char.repeat(size))){
+      accumString += char;
+    }
+    return accumString;
+  }, firstSlice);
 }
